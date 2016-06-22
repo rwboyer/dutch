00c93ea0b65652ecf693cf7e2b4bd35f25f80968
Robert W Boyer <rwboyer@mac.com>
2016-06-22 19:16:12 -0400

    fix double jQuery loading… for some reason table press is loading jQuery again in the footer. Disable it loading in header with theme.

--- library/enqueue-scripts.php
+++ library/enqueue-scripts.php
@@ -17,7 +17,7 @@ if ( ! function_exists( 'foundationpress_scripts' ) ) :
 	wp_enqueue_style( 'main-stylesheet', get_stylesheet_directory_uri() . '/assets/stylesheets/foundation.css' );
 
 	// Deregister the jquery version bundled with WordPress.
-	wp_deregister_script( 'jquery' );
+	//wp_deregister_script( 'jquery' );
 
 	// Modernizr is used for polyfills and feature detection. Must be placed in header. (Not required).
 	wp_enqueue_script( 'modernizr', get_template_directory_uri() . '/assets/javascript/vendor/modernizr.js', array(), '2.8.3', false );
@@ -26,7 +26,7 @@ if ( ! function_exists( 'foundationpress_scripts' ) ) :
 	wp_enqueue_script( 'fastclick', get_template_directory_uri() . '/assets/javascript/vendor/fastclick.js', array(), '1.0.0', false );
 
 	// CDN hosted jQuery placed in the header, as some plugins require that jQuery is loaded in the header.
-	wp_enqueue_script( 'jquery', '//ajax.googleapis.com/ajax/libs/jquery/2.1.0/jquery.min.js', array(), '2.1.0', false );
+	//wp_enqueue_script( 'jquery', '//ajax.googleapis.com/ajax/libs/jquery/2.1.0/jquery.min.js', array(), '2.1.0', false );
 
 	// If you'd like to cherry-pick the foundation components you need in your project, head over to Gruntfile.js and see lines 67-88.
 	// It's a good idea to do this, performance-wise. No need to load everything if you're just going to use the grid anyway, you know :)
